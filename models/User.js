0b96e6246f9671c8b75c93c20def3b0ddb041f14
Narmeen Humayon <bscs22013@itu.edu.pk>
2024-12-24 12:42:37 +0530

    JWT token added

--- models/User.js
+++ models/User.js
@@ -3,6 +3,19 @@ const bcrypt = require("bcryptjs");
 
 
 /* using mongodb validations */
+/*
+Form LOGIN: format:
+email
+password
+
+form REGISTER format:
+First Name
+Last Name 
+Email
+Password
+Confirm Password
+password and confirm paswrod must match 
+*/
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -48,3 +61,56 @@ module.exports = mongoose.model("User", UserSchema);
 npm i bcrypt
 o hash passwords, bcrypt’s password hashing algorithm combines the password string, salt, and the cost to derive a 24-byte hash using base 64 encoding.
 */
+
+/*
+I am using 
+bcrypt.salt()
+bcrypt.compare() to check if login password matches to password input
+bcrypt.hash() to 
+
+example usages from website: stackoverflow.com 
+//required files
+const express = require('express')
+const router = express.Router();
+
+//bcryptjs
+const bcrypt = require('bcryptjs')
+
+//User modal of mongoDB
+const User = require('../../models/User')
+
+
+//Post request for login
+router.post('/', (req, res) => {
+    //email and password
+    const email = req.body.email
+    const password = req.body.password
+
+    //find user exist or not
+    User.findOne({ email })
+        .then(user => {
+            //if user not exist than return status 400
+            if (!user) return res.status(400).json({ msg: "User not exist" })
+
+            //if user exist than compare password
+            //password comes from the user
+            //user.password comes from the database
+            bcrypt.compare(password, user.password, (err, data) => {
+                //if error than throw error
+                if (err) throw err
+
+                //if both match than you can do anything
+                if (data) {
+                    return res.status(200).json({ msg: "Login success" })
+                } else {
+                    return res.status(401).json({ msg: "Invalid credencial" })
+                }
+
+            })
+
+        })
+
+})
+
+module.exports = router
+ */
